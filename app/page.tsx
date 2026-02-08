e2a86c676d51627f3cfcfd404a392e4694c0e800
shiedz <djshied25@example.com>
2026-02-08 23:47:49 +0800

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,92 @@
-import Image from "next/image";
+"use client";
+import { useState } from "react";
 
 export default function Home() {
+  const [yesClicked, setYesClicked] = useState(false);
+  const [noStyle, setNoStyle] = useState({});
+
+  const moveNoButton = () => {
+    const x = Math.random() * 200 - 100;
+    const y = Math.random() * 200 - 100;
+    setNoStyle({ transform: `translate(${x}px, ${y}px)` });
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+    <div className="min-h-screen p-4 bg-gradient-to-br from-black-300 to-red-800 overflow-hidden">
+      <main className="min-h-screen flex items-center justify-center">
+        <div className="bg-white rounded-3xl shadow-2xl p-8 text-center max-w-sm w-full animate-in zoom-in md:max-w-3xl md:p-16">
+          {!yesClicked ? (
+            <>
+              <h1 className="text-3xl font-bold text-pink-500 mb-2 md:text-7xl">
+                Will you be my Valentine? 💘
+              </h1>
+              <p className="text-gray-600 m-4 text-3xl md:m-8">Feb. 14, 2026</p>
+
+              <div className="flex justify-center gap-4 relative">
+                <button
+                  onClick={() => setYesClicked(true)}
+                  className="px-6 py-2 max-w-[200] w-full block h-12 rounded-full bg-pink-500 text-white font-semibold hover:scale-105 transition cursor-pointer"
+                >
+                  Yes 💖
+                </button>
+
+                <button
+                  onMouseEnter={moveNoButton}
+                  style={noStyle}
+                  className="px-6 py-2 max-w-[200] w-full block h-12 rounded-full bg-gray-200 text-gray-700 transition"
+                >
+                  No 🙃
+                </button>
+              </div>
+            </>
+          ) : (
+            <>
+              <h2 className="text-4xl font-bold text-pink-500 mb-2 md:text-6xl">
+                YAY 🥰 <span className="block m-5">HA-VAL-DAY!!</span>
+              </h2>
+              <p className="text-gray-600 text-3xl">
+                You just made my heart very happy 💕
+              </p>
+              <a
+                  href="/"
+                  className="px-6 py-2 max-w-[200] w-full block h-12 leading-8 mx-auto mt-5 rounded-full bg-pink-500 text-white font-semibold hover:scale-105 transition cursor-pointer"
+                >
+                  Lezgaur!
+                </a>
+              <FloatingHearts />
+            </>
+          )}
         </div>
       </main>
+      <footer className="text-center p-4">
+        Copyright &copy; 2026 | Made with Love
+      </footer>
+    </div>
+  );
+}
+
+function FloatingHearts() {
+  const emojis = ["💖", "💕", "💐",, "❤️", "🌸"];
+
+  return (
+    <div className="pointer-events-none fixed inset-0 overflow-hidden">
+      {Array.from({ length: 80 }).map((_, i) => {
+        const emoji = emojis[Math.floor(Math.random() * emojis.length)];
+        return (
+          <span
+            key={i}
+            className="absolute bottom-0 animate-float"
+            style={{
+              left: Math.random() * 100 + "vw",
+              animationDuration: 2 + Math.random() * 4 + "s",
+              fontSize: 14 + Math.random() * 32,
+              animationDelay: Math.random() * 2 + "s",
+            }}
+          >
+            {emoji}
+          </span>
+        );
+      })}
     </div>
   );
 }
